c5bd2f9eec8615612b54d876047fa2c21b189460
Marcos Willian <marcoswillianl1987@gmail.com>
2024-11-26 14:28:15 -0300

    Rodapé
    Falta só o rodapé

--- script/script.js
+++ script/script.js
@@ -1,18 +1,6 @@
 const botaoMenu = document.querySelector('#botao-menu');
 const listaMenu = document.querySelector('#lista-menu');
 
-function exibirMenu() {
-    botaoMenu.addEventListener('click', () => {
-        if (listaMenu.classList.contains('hidden')) {
-            listaMenu.classList.remove('hidden');
-            requestAnimationFrame(() => {
-                listaMenu.classList.add('opacity-100');
-            });
-        } else {
-            listaMenu.classList.remove('opacity-100');
-            listaMenu.addEventListener('transitionend', () => listaMenu.classList.add('hidden'), { once: true });
-        }
-    });
-}
-
-exibirMenu();
+botaoMenu.addEventListener('click', () => {
+  listaMenu.classList.toggle('hidden'); // Alterna a visibilidade do menu
+});
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        screens: {
+          'custom-656': '656px',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
